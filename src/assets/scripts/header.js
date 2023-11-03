588bfecb02ce9f8ef24b5c866ed78e403eb35433
Alexander Eryomenko <sasha.eryomenko77@gmail.com>
2023-11-03 11:57:36 +0600

    feat: header

--- src/assets/scripts/header.js
+++ src/assets/scripts/header.js
@@ -1,23 +1,46 @@
+// hamburger
 const hamburger = document.querySelector('.hamburger')
 
-// hamburger trigger
 hamburger.addEventListener('click', () => {
     if (hamburger.classList.contains('active')) {
         hamburger.classList.remove('active')
+        document.querySelectorAll('.accordion-item.active').forEach(activeItem => {
+            activeItem.classList.remove('active');
+            activeItem.querySelector('.accordion-content').style.maxHeight = null;
+        });
     } else {
         hamburger.classList.add('active')
     }
 })
 
 // accordion
-const accordionItems = document.querySelectorAll('.accordion-item')
+let accordionHeaders = document.querySelectorAll('.accordion-header');
 
-accordionItems.forEach((accordionItem) => {
-    accordionItem.addEventListener('click', () => {
-        if (accordionItem.classList.contains('active')) {
-            accordionItem.classList.remove('active')
-        } else {
-            accordionItem.classList.add('active')
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', function() {
+        let accordionItem = header.parentElement;
+        let accordionContent = accordionItem.querySelector('.accordion-content');
+
+        // Текущий активный элемент
+        let currentActive = document.querySelector('.accordion-item.active');
+
+        // Если активный элемент существует и не равен текущему элементу, удаляем класс 'active' и сбрасываем max-height
+        if (currentActive && currentActive !== accordionItem) {
+            currentActive.classList.remove('active');
+            currentActive.querySelector('.accordion-content').style.maxHeight = null;
         }
-    })
-})
+
+        if (!accordionContent) return
+
+        // Переключаем состояние текущего элемента
+        accordionItem.classList.toggle('active');
+        if (accordionItem.classList.contains('active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+        } else {
+            accordionContent.style.maxHeight = null;
+        }
+    });
+});
+
+
+
